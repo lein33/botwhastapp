b1af4c3ad07103c9fe31970ecd7ffc758cb38be6
lein33 <sleandrojoel@hotmail.com>
2023-02-13 23:53:01 -0500

    webhook response

--- message/business/views.py
+++ message/business/views.py
@@ -25,4 +25,23 @@ def whatsAppWebhook(request):
 
         if request.method == 'POST':
             data = json.loads(request.body)
+            if 'object' in data and 'entry' in data:
+                if data['object']=='whatsapp_business_account':
+                    try:
+                        for entry in data['entry']:
+                            phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
+                            phoneId = entry['changes'][0]['value']['metadata']['phone_number_id']
+                            profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
+                            whatsaAppId = entry['changes'][0]['value']['contacts'][0]['wa_id']
+                            fromId = entry['changes'][0]['value']['messages'][0]['from']
+                            messageId = entry['changes'][0]['value']['messages'][0]['id']
+                            timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
+                            text = entry['changes'][0]['value']['messages'][0]['text']['body']
+
+                            phoneNumber="593990137716"
+                            message = 'RE: {} was received'.format(text)
+                            sendWhatsAppMessage(phoneNumber,message)
+                    except:
+                        pass
             return HttpResponse('success',status=200)
+        
