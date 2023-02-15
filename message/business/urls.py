aad3c422e271e62e0bf246545637d0ea9f7ed186
lein33 <sleandrojoel@hotmail.com>
2023-02-14 22:15:52 -0500

    webhook update

--- message/business/urls.py
+++ message/business/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.home,name='home'),
-	path('8bfcb0b1-c2c3-441d-8184-c1e33fb7b5b6',views.whatsAppWebhook,name='whatsapp-webhook'),
+	path('b390b018-e0fb-4fc7-9550-df6a53c7e962',views.whatsAppWebhook,name='whatsapp-webhook'),
 
 ]
 #https://botwhatsappdemoleo.store/b390b018-e0fb-4fc7-9550-df6a53c7e962
