cb545e02436f86a1d5e819a4f511b1fb634499d0
lein33 <sleandrojoel@hotmail.com>
2023-02-20 13:43:46 -0500

    ia

--- message/business/aigenerations.py
+++ message/business/aigenerations.py
@@ -3,10 +3,10 @@ import openai
 from django.conf import settings
 openai.api_key = settings.OPENAI_API_KEY
 
-def companyDescription(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,años,progreso):
+def descripcion_ompania_progreso(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,anos,progreso):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Generar la  descripción de la empresa para un plan de negocios para el siguiente negocio, utilizando las pautas proporcionadas:\nNombre comercial: {}\nTipo de negocio: {}\nPaís: {}\nProducto o servicio: {}\nDescripción breve del negocio: {}\nAños en operación: {}\nProgreso comercial hasta la fecha: {}\n\n\nDirectrices: Comience la descripción de la empresa escuchando el nombre comercial y la estructura de la empresa. si se proporciona. Escriba una descripción comercial detallada para la descripción del tipo proporcionada, en un tono comercial profesional. Describa la industria en la que operará el negocio y reescriba el progreso del negocio hasta la fecha.\nFinalmente, proporcione tres objetivos de negocio numerados adecuados para este negocio y para cada objetivo, describa cómo el objetivo se ajusta al negocio y cómo beneficiará a las partes interesadas a largo plazo.".format(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,años,progreso),
+        prompt="Generar la  descripcion de la empresa para un plan de negocios para el siguiente negocio, utilizando las pautas proporcionadas:\nNombre comercial: {}\nTipo de negocio: {}\nPaís: {}\nProducto o servicio: {}\nDescripción breve del negocio: {}\nAños en operación: {}\nProgreso comercial hasta la fecha: {}\n\n\nDirectrices: Comience la descripción de la empresa escuchando el nombre comercial y la estructura de la empresa. si se proporciona. Escriba una descripción comercial detallada para la descripción del tipo proporcionada, en un tono comercial profesional. Describa la industria en la que operará el negocio y reescriba el progreso del negocio hasta la fecha.\nFinalmente, proporcione tres objetivos de negocio numerados adecuados para este negocio y para cada objetivo, describa cómo el objetivo se ajusta al negocio y cómo beneficiará a las partes interesadas a largo plazo.".format(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,anos,progreso),
         temperature=0.33,
         max_tokens=2000,
         top_p=1,
@@ -18,7 +18,47 @@ def companyDescription(nombre_empresa,tipo_negocio,pais,producto_servicio,descri
     if 'choices' in response:
         if len(response['choices'])>0:
             answer = response['choices']
-            return answer
+            return nombre_empresa+answer
+        else:
+            return ''
+    else:
+        return ''
+    
+def descripcion_compania(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,anos):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Generar la  descripcion de la empresa para un plan de negocios para el siguiente negocio, utilizando las pautas proporcionadas:\nNombre comercial: {}\nTipo de negocio: {}\nPaís: {}\nProducto o servicio: {}\nDescripción breve del negocio: {}\nAños en operación: {}\n\n\nDirectrices: Comience la descripción de la empresa escuchando el nombre comercial y la estructura de la empresa. si se proporciona. Escriba una descripción comercial detallada para la descripción del tipo proporcionada, en un tono comercial profesional. Describa la industria en la que operará el negocio y reescriba el progreso del negocio hasta la fecha.\nFinalmente, proporcione tres objetivos de negocio numerados adecuados para este negocio y para cada objetivo, describa cómo el objetivo se ajusta al negocio y cómo beneficiará a las partes interesadas a largo plazo.".format(nombre_empresa,tipo_negocio,pais,producto_servicio,descripcion_corta,anos),
+        temperature=0.33,
+        max_tokens=2000,
+        top_p=1,
+        best_of=2,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    if 'choices' in response:
+        if len(response['choices'])>0:
+            answer = response['choices'][0]['text'].replace('\n','<br/>')
+            return nombre_empresa+answer
+        else:
+            return ''
+    else:
+        return ''
+
+def AnalisiMercado(nombre_empresa, producto_servicio,descripcion_corta):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Genere un análisis de mercado para un plan de negocios, para el siguiente negocio utilizando las pautas proporcionadas:\nnombre de la empresa: {}\nproducto o servicio: {}\ndesdripcion corta: {}.\n\nDirectrices: Describir cómo posicionar el negocio en la industria para ser competitivo y exitoso. Evaluar minuciosamente el mercado actual en el que operará la empresa. Responda las siguientes preguntas. (1) ¿Quiénes son los clientes potenciales para el negocio? (2) ¿Cuáles son los hábitos de compra de los clientes?. (3) ¿Qué tan grande es el mercado objetivo?. (4) ¿Cuánto están dispuestas a pagar las personas por el producto?. (5) ¿Quiénes son los principales competidores? y (6) ¿Cuáles son mis competidores fortalezas y debilidades?.\n".format(nombre_empresa, producto_servicio,descripcion_corta),
+    temperature=0.7,
+    max_tokens=2000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+    if 'choices' in response:
+        if len(response['choices'])>0:
+            answer = response['choices'][0]['text'].replace('\n','<br/>')
+            return nombre_empresa+answer
         else:
             return ''
     else:
--- message/business/models.py
+++ message/business/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.utils import timezone
+from uuid import uuid4
+from django.conf import settings
+import os
 
-# Create your models here.
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(null=True, blank=True,max_length=100)
+    phoneId = models.CharField(null=True, blank=True,max_length=100)
+    
+    uniqueId = models.CharField(null=True, blank=True,max_length=100)
+    date_created = models.DateTimeField(blank=True,null=True)
+    last_updated = models.DateTimeField(blank=True,null=True)
+
+    def save(self,*args,**kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4().split('-')[4])
+        
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Profile, self).save(*args,**kwargs)
+
+class PlanEmpresarial(models.Model):
+    perfil = models.ForeignKey(Perfil,on_delete=models.CASCADE,null=True,blank=True)
+    descripcion_compania = models.TextField(null=True, blank=True)
+    analisis_mercado = models.TextField(null=True, blank=True)
+    analisis_foda = models.TextField(null=True, blank=True)
+    detalle_producto = models.TextField(null=True, blank=True)
+    strategia_marketing = models.TextField(null=True, blank=True)
+
+    uniqueId = models.CharField(null=True,blank=True,unique=True,max_length=100)
+    fecha_creacion = models.DateTimeField(blank=True,null=True)
+    ultima_edicion = models.DateTimeField(blank=True,null=True)
+    
+    def save(self,*args,**kwargs):
+        if self.fecha_creacion is None:
+            self.fecha_creacion = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4().split('-')[4])
+        
+        self.ultima_edicion = timezone.localtime(timezone.now())
+        super(Perfil, self).save(*args,**kwargs)
+
+
+
+
