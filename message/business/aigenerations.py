12cecf7bcae406bbbd1040f29d02c024e80a192e
lein33 <sleandrojoel@hotmail.com>
2023-02-20 06:37:29 -0500

    ia

--- message/business/aigenerations.py
+++ message/business/aigenerations.py
@@ -6,7 +6,7 @@ openai.api_key = settings.OPENAI_API_KEY
 def companyDescription():
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="que es chatgpt"
+        prompt="Generar la  descripción de la empresa para un plan de negocios para el siguiente negocio, utilizando las pautas proporcionadas:\nNombre comercial: Kijima Shoes\nTipo de negocio: Pty Ltd\nPaís: Sudáfrica\nProducto o servicio: zapatillas para correr\nDescripción breve del negocio: Producimos zapatillas de élite para hombre y mujer.\nAños en operación: 1 año\nProgreso comercial hasta la fecha: todavía no hemos vendido zapatos\n\n\nDirectrices: Comience la descripción de la empresa escuchando el nombre comercial y la estructura de la empresa. si se proporciona. Escriba una descripción comercial detallada para la descripción del tipo proporcionada, en un tono comercial profesional. Describa la industria en la que operará el negocio y reescriba el progreso del negocio hasta la fecha.\nFinalmente, proporcione tres objetivos de negocio numerados adecuados para este negocio y para cada objetivo, describa cómo el objetivo se ajusta al negocio y cómo beneficiará a las partes interesadas a largo plazo.",
         temperature=0.33,
         max_tokens=2000,
         top_p=1,
