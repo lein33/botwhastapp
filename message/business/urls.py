f26b76756badb19ea334628197dc0280fea31f83
lein33 <sleandrojoel@hotmail.com>
2023-02-20 20:59:19 -0500

    ia

--- message/business/urls.py
+++ message/business/urls.py
@@ -5,9 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
-	path('2b3ed38e-5527-4176-8541-5420fb01e20f',views.whatsAppWebhook,name='whatsapp-webhook'),
+	path('test',views.whatsAppWebhook,name='whatsapp-webhook'),
 
 ]
-#https://botwhatsappdemoleo.store/b390b018-e0fb-4fc7-9550-df6a53c7e962
+#https://botwhatsappdemoleo.store/
 
-#token=26ce5829-2910-4301-8b62-7e8c99ffb33c
