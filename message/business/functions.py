d1add5212b044a951b128f5725618b94adde5cef
lein33 <sleandrojoel@hotmail.com>
2023-02-21 23:56:34 -0500

    probando odf

--- message/business/functions.py
+++ message/business/functions.py
@@ -111,8 +111,8 @@ def createNewBusinessPlan(chat):
     message='Your business \n \n{}'.format(doc_url)
     sendWhatsAppMessage(chat.perfil.phoneNumber,  message)
     chat.delete()
-    user = User.objects.filter(username=chat.perfil.user)
-    user.delete()
+    userdel = User.objects.filter(username=chat.perfil.user)
+    userdel.delete()
 def handleWhatsAppChat(fromId, profileName, phoneId,text):
     try:
         chat = ChatSessions.objects.get(perfil__phoneNumber=fromId)
